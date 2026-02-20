73c0401911aa863c972b607701124f1769f15d92
torresaa4 <torresaa44@gmail.com>
2026-02-20 15:15:29 -0800

    Add files via upload

---
+++ frontend/dashboard.kt
@@ -0,0 +1,9 @@
+package com.example.cse4550_login
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DashboardScreen(){
+    Text(text = "Hello")
+}
