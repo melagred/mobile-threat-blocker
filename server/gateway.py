75a4a373789ffc03f9300a71249bb4fc49ce7c44
MeowterSpace5129 <33559950+MeowterSpace5129@users.noreply.github.com>
2026-03-16 19:46:25 -0700

    add tcp server to gateway

--- server/gateway.py
+++ server/gateway.py
@@ -1,4 +1,5 @@
 import struct
+import socketserver
 from fcntl import ioctl
 
 def openTun(tunName):
@@ -16,3 +17,35 @@ tun = openTun(b"tun0")
 tun.write(syn)
 reply = tun.read(1024)
 print(repr(reply))
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        pieces = [b'']
+        total = 0
+        while b'\n' not in pieces[-1] and total < 10_000:
+            pieces.append(self.request.recv(2000))
+            total += len(pieces[-1])
+        self.data = b''.join(pieces)
+        print(f"Received from {self.client_address[0]}:")
+        print(self.data.decode("utf-8"))
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+        # after we return, the socket will be closed.
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
