7c5da948764631f057d379ddbe4699ed85960ae4
torresaa4 <torresaa44@gmail.com>
2026-03-18 12:32:43 -0700

    Update MainActivity.kt

--- frontend/MainActivity.kt
+++ frontend/MainActivity.kt
@@ -2,24 +2,38 @@ package com.example.cse4550_login
 
 import android.Manifest
 import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.cse4550_login.ApiClient.checkDomain
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        ApiClient.checkDomain("example.com") { result ->
+            Log.d("API_Test", "Response: $result")
+        }
+
+
         if (Build.VERSION.SDK_INT >= 33) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
                 != PackageManager.PERMISSION_GRANTED
@@ -41,9 +55,14 @@ class MainActivity : ComponentActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+            //start
             val startIntent = Intent(this, SafetyFirstVpnService::class.java)
             startIntent.action = SafetyFirstVpnService.ACTION_START
-            startService(startIntent)
+            ContextCompat.startForegroundService(this, startIntent)
+
+            // update
+            val vm: LoginsViewModel = ViewModelProvider(this)[LoginsViewModel::class.java]
+            vm.setVpnOn(true)
         }
     }
 
@@ -76,6 +95,9 @@ fun AppNavigation() {
                 },
                 SettingsClick = {
                     navController.navigate(Routes.SettingScreen)
+                },
+                ThreatsLogClick = {
+                    navController.navigate(Routes.ThreatLogScreen)
                 }
             )
         }
@@ -91,6 +113,20 @@ fun AppNavigation() {
                 AboutClick = {
                     navController.navigate(Routes.AboutScreen)
                 })
+        }
+
+        composable(Routes.ThreatLogScreen){
+            ThreatLogScreen(
+                DashsClick = {
+                    navController.navigate(Routes.DashboardScreen)
+                },
+                ThreatsClick = {
+                    navController.navigate(Routes.ThreatsScreen)
+                },
+                SettingsClick = {
+                    navController.navigate(Routes.SettingScreen)
+                }
+            )
         }
 
         composable(Routes.AboutScreen){
@@ -109,7 +145,4 @@ fun AppNavigation() {
 
     }
 }
-
-
-
 
