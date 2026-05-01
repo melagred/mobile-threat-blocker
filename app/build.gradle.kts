04917b3a122faae0a7d6a79c4c7b0948ef4738a4
MeowterSpace5129 <33559950+MeowterSpace5129@users.noreply.github.com>
2026-05-01 00:59:44 -0700

    use gradle jvm toolchain instead of overrides

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
 }
-
+kotlin {
+    jvmToolchain(19)
+}
 android {
     namespace = "com.example.safetyfirst"
     compileSdk {
@@ -33,11 +35,6 @@ android {
     buildFeatures {
         compose = true
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.15"
     }
@@ -58,12 +55,20 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
 
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.foundation:foundation:1.10.6")
+    implementation("androidx.compose.material3:material3:1.4.0")
+    implementation("androidx.room:room-runtime:2.8.4")
 
+    implementation("androidx.startup:startup-runtime:1.2.0")
+    implementation("androidx.activity:activity-compose:1.13.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+    implementation("androidx.navigation:navigation-ui:2.9.7")
+    implementation("androidx.navigation:navigation-compose:2.9.7")
+
+    implementation("com.squareup.okhttp3:okhttp:5.3.0")
     // Android Studio Preview support
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
-
-    implementation("androidx.startup:startup-runtime:1.2.0")
 
 
 }
